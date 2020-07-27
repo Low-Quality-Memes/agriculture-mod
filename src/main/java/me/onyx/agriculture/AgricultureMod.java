b9d0adebbeac745cb9958dc69e2085515cf79f02
Low-Quality-Memes <notsirspiff@gmail.com>
2020-07-27 15:21:50 -0500

    add corn

--- src/main/java/me/onyx/agriculture/AgricultureMod.java
+++ src/main/java/me/onyx/agriculture/AgricultureMod.java
@@ -1,14 +1,22 @@
 package me.onyx.agriculture;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class AgricultureMod implements ModInitializer {
+
+    private static final Item CORN = new CornItem(new Item.Settings().group(ItemGroup.FOOD).maxCount(64));
+
 	@Override
 	public void onInitialize() {
+	    Registry.register(Registry.ITEM, new Identifier("agriculture", "corn"), CORN);
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		System.out.println("Hello Fabric world!");
+		System.out.println("bean time");
 	}
 }
---
+++ src/main/java/me/onyx/agriculture/CornItem.java
@@ -0,0 +1,9 @@
+package me.onyx.agriculture;
+
+import net.minecraft.item.Item;
+
+public class CornItem extends Item {
+    public CornItem(Settings settings) {
+        super(settings);
+    }
+}
