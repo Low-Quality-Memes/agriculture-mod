8f09baf8d4ae846ac6ca9f686da9aebc5c5c0969
Low-Quality-Memes <notsirspiff@gmail.com>
2020-07-28 10:59:07 -0500

    corn is now a thing

--- src/main/java/me/onyx/agriculture/AgricultureMod.java
+++ src/main/java/me/onyx/agriculture/AgricultureMod.java
@@ -1,24 +1,31 @@
 package me.onyx.agriculture;
 
-import me.onyx.agriculture.items.CornItem;
+import me.onyx.agriculture.crops.CornCrop;
 import me.onyx.agriculture.items.StrawberryItem;
 import me.onyx.agriculture.items.TomatoItem;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.CropBlock;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class AgricultureMod implements ModInitializer {
 
-    private static final Item CORN = new CornItem(new Item.Settings().group(ItemGroup.FOOD).maxCount(64));
-    private static final Item TOMATO = new TomatoItem(new Item.Settings().group(ItemGroup.FOOD).maxCount(64));
-    private static final Item STRAWBERRY = new StrawberryItem(new Item.Settings().group(ItemGroup.FOOD).maxCount(64));
+    public static final Item TOMATO = new TomatoItem(new Item.Settings().group(ItemGroup.FOOD).maxCount(64));
+    public static final Item STRAWBERRY = new StrawberryItem(new Item.Settings().group(ItemGroup.FOOD).maxCount(64));
+    public static final CropBlock CORN_CROP = new CornCrop(AbstractBlock.Settings.of(Material.PLANT).noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.CROP));
 	@Override
 	public void onInitialize() {
-	    Registry.register(Registry.ITEM, new Identifier("agriculture", "corn"), CORN);
         Registry.register(Registry.ITEM, new Identifier("agriculture", "tomato"), TOMATO);
         Registry.register(Registry.ITEM, new Identifier("agriculture", "strawberry"), STRAWBERRY);
+        Registry.register(Registry.BLOCK, new Identifier("agriculture", "corn_crop"), CORN_CROP);
+        Registry.register(Registry.ITEM, new Identifier("agriculture", "corn_crop"), new BlockItem(CORN_CROP, new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(3).saturationModifier(6f).build())));
 
 		System.out.println("bean time");
 	}
--- src/main/java/me/onyx/agriculture/items/CornItem.java
+++
@@ -1,9 +0,0 @@
-package me.onyx.agriculture.items;
-
-import net.minecraft.item.Item;
-
-public class CornItem extends Item {
-    public CornItem(Settings settings) {
-        super(settings);
-    }
-}
